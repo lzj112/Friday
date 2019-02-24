c2d7dc4f79576fe9ee74f4f51b6ac81387968ea2
lzj112 <872575628@qq.com>
2019-02-24 23:58:14 +0800

    why!

--- Examples/Most_connection/Client.cpp
+++ Examples/Most_connection/Client.cpp
@@ -22,25 +22,30 @@ int main()
     netAdderss.sin_family = AF_INET;
     netAdderss.sin_port = htons(port);
     inet_pton(AF_INET, ip, &netAdderss.sin_addr);
-
-    // pid_t t[3];
+    
+    pid_t t[3];
     // for (int i = 0; i < 3; i++) 
     // {
-    //     t[i] = fork();
-    //     if (t[i] == 0) 
-    //     {
-            array<int, 1000> sockfd;
-            for (int i = 0; i < 1000; i++)
+        t[0] = fork();
+        if (t[0] == 0) 
+        {
+            printf("开始创建连接\n");
+            vector<int> sockfd;
+            for (int i = 0; ; i++)
             {
-                sockfd[i] = socket(AF_INET, SOCK_STREAM, 0);
-                // int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+                int socketfd = socket(AF_INET, SOCK_STREAM, 0);
                 // assert(socketfd != -1);
+                if (socketfd == -1)
+                {
+                    perror("socket  --->because of : ");
+                }
+                sockfd.push_back(socketfd);
                 printf("create socket = %d\n", sockfd[i]);
-                // sockfd.push_back(socketfd);
                 int ret = connect(sockfd[i], (sockaddr *)&netAdderss, addrLength);
                 if (ret == 0)
                 {
-                    printf("%d connect succeed\n", sockfd[i]);
+                    printf("%d connect succeed\n", socketfd);
+                    // printf("%d connect succeed\n", sockfd[i]);
                     // char ctr[] = "hello";
                     // PackageTCP pac(1, ctr);
                     // ret = send(sockfd[i], &pac, sizeof(PackageTCP), 0);
@@ -48,13 +53,15 @@ int main()
                     //     printf("send failed\n");
                 }
                 else 
-                    printf("%dconnect failed\n", sockfd[i]);
-    //         }
-    //     }
-    //     else 
-    //     ;
+                {
+                    printf("%dconnect failed\n", socketfd);
+                    perror("because of : ");
+                    break;
+                }
             }
+        // }
+    }
 
-    // while (1) {}
+    while (1) {}
 
 }
