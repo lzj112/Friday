19455c05ad027f32c08469739371843204e06db8
lzj112 <872575628@qq.com>
2019-04-28 20:36:39 +0800

    .

---
+++ Examples/pingpong/build/src/CMakeFiles/FridayNET.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FridayNET.dir/Connector.cc.o"
+  "CMakeFiles/FridayNET.dir/EpollBase.cc.o"
+  "CMakeFiles/FridayNET.dir/EpollEventLoop.cc.o"
+  "CMakeFiles/FridayNET.dir/FServer.cc.o"
+  "CMakeFiles/FridayNET.dir/IOBuffer.cc.o"
+  "CMakeFiles/FridayNET.dir/IOThread.cc.o"
+  "CMakeFiles/FridayNET.dir/IOThreadPool.cc.o"
+  "CMakeFiles/FridayNET.dir/MyEvent.cc.o"
+  "CMakeFiles/FridayNET.dir/SocketTCP.cc.o"
+  "CMakeFiles/FridayNET.dir/Timer.cc.o"
+  "CMakeFiles/FridayNET.dir/TimerWheel.cc.o"
+  "libFridayNET.pdb"
+  "libFridayNET.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FridayNET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
